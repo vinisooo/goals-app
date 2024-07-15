354563f460b71569f05392398f8121b5563cf8c2
vinisooo <cviniciusliramdev@gmail.com>
2024-07-15 13:29:23 -0300

    create card adapter with recycler view, create card xml style

---
+++ app/src/main/java/com/example/goals/adapters/GoalCardAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.goals.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.goals.databinding.ActivityGoalsBinding
+import com.example.goals.databinding.GoalCardBinding
+import com.example.goals.models.Goal
+
+class GoalCardAdapter (private val context: Context, private val itemList: MutableList<Goal>): RecyclerView.Adapter<GoalCardAdapter.ViewHolder>() {
+    inner class ViewHolder(private val binding: GoalCardBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(goal: Goal) {
+            binding.goalCard.setOnClickListener {
+                TODO("Implement intent")
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalCardAdapter.ViewHolder {
+        val binding = GoalCardBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: GoalCardAdapter.ViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    override fun getItemCount(): Int = itemList.size
+}
